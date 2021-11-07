98bd17fd0ebaf4075f0290829343db50d251ada9
hoangnam1402 <18521130@gm.uit.edu.vn>
2021-11-07 13:34:31 +0700

    connect DB

--- index.js
+++ index.js
@@ -15,6 +15,20 @@ app.get('/',(req, res) =>{
     res.send('Welcome');
 })
 
+const MONGO_URI=`mongodb+srv://${process.env.dbUserName}:${process.env.dbPw}@cluster0.mbklg.mongodb.net/${process.env.dbName}`;
+const connectBD = async () => {
+    try {
+        await mongoose.connect(MONGO_URI);
+        console.log('Connect success');
+    } catch (error){
+        console.log('Connect fail')
+    }
+}
+
+connectBD();
+
+
+
 app.listen(port,()=>{
     console.log(`Listening at http://localhost:${port}`)
 })
